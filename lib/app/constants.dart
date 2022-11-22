69afd29417a205d2622db6f65bd5ceee098b1b8f
Md Didarul islam <islamdidarmd@gmail.com>
2022-11-22 23:38:42 +0600

    Change Android App Name

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,10 +1,10 @@
 import 'package:workout_buddy_finder/app/env.dart';
 
-const defaultAppName = 'Flutter App';
+const defaultAppName = 'Workout Buddy Finder';
 const defaultEnv = EnvType.Development;
 
-const devAppName = 'Flutter App Dev';
+const devAppName = 'Workout Buddy Finder Dev';
 const devEnv = EnvType.Development;
 
-const prodAppName = 'Flutter App';
+const prodAppName = 'Workout Buddy Finder';
 const prodEnv = EnvType.Production;
