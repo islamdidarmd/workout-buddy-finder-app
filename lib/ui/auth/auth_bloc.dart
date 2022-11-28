5454277af5c1639ba8382767579e044fab59a3f9
Md Didarul islam <islamdidarmd@gmail.com>
2022-11-28 23:38:52 +0600

    Create Auth Bloc

---
+++ lib/ui/auth/auth_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(AuthInitialState()) {
+    on<AuthEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
