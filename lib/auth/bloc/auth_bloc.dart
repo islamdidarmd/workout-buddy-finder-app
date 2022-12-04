eb7cca4bcdf2ba7605defdf5464ee6d7ad86cb0d
Md Didarul islam <islamdidarmd@gmail.com>
2022-12-04 22:26:26 +0600

    Add auth module to service locator

--- lib/auth/bloc/auth_bloc.dart
+++ lib/auth/bloc/auth_bloc.dart
@@ -1,10 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:injectable/injectable.dart';
 
 part 'auth_event.dart';
 
 part 'auth_state.dart';
 
+@injectable
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitialState()) {
     on<AuthSignInWithGoogleEvent>((event, emit) async {
