ea469b005df03705f28794e36f9ed5f489581b0f
Md Didarul islam <islamdidarmd@gmail.com>
2022-12-10 13:20:43 +0600

    Add profile picture in profile page

--- lib/auth/domain/entity/app_user.dart
+++ lib/auth/domain/entity/app_user.dart
@@ -1,19 +1,25 @@
 class AppUser {
   final String userId;
   final DateTime registered;
-  String? name;
-  String? email;
-  String? profilePicture;
+  final String name;
+  final String email;
+  final String profilePicture;
 
   bool get isEmpty => userId.isEmpty;
 
   AppUser({
     required this.userId,
     required this.registered,
-    this.name,
-    this.email,
-    this.profilePicture,
+    required this.name,
+    required this.email,
+    required this.profilePicture,
   });
 
-  factory AppUser.empty() => AppUser(userId: '', registered: DateTime.now());
+  factory AppUser.empty() => AppUser(
+        userId: '',
+        registered: DateTime.now(),
+        name: '',
+        email: '',
+        profilePicture: '',
+      );
 }
