347fc842a6b2c0d9afa60da3ca6133e0e61a43ef
Md Didarul islam <islamdidarmd@gmail.com>
2022-12-19 21:48:56 +0600

    Add intro in suggestions page

--- lib/core/ui/style/texts.dart
+++ lib/core/ui/style/texts.dart
@@ -1,11 +1,27 @@
-import 'dart:ui';
+// This is a helper file for creating different text styles.
+// ignore_for_file: avoid-returning-widgets
 
 import 'package:flutter/material.dart';
+
+Text mediumBody(context, String text) => Text(
+      text,
+      style: Theme.of(context)
+          .textTheme
+          .bodyMedium
+          ?.copyWith(fontWeight: FontWeight.bold),
+    );
 
 Text mediumBoldTitle(context, String text) => Text(
       text,
       style: Theme.of(context)
           .textTheme
           .titleMedium
+          ?.copyWith(fontWeight: FontWeight.bold),
+    );
+Text largeBoldTitle(context, String text) => Text(
+      text,
+      style: Theme.of(context)
+          .textTheme
+          .titleLarge
           ?.copyWith(fontWeight: FontWeight.bold),
     );
