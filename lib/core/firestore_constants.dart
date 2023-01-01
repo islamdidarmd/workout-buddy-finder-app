fed39e122cc06c12c0900c8489d4a5e9f213bffb
Md Didarul islam <islamdidarmd@gmail.com>
2023-01-01 20:56:50 +0600

    Add like and dislike option

--- lib/core/firestore_constants.dart
+++ lib/core/firestore_constants.dart
@@ -6,7 +6,3 @@ const String disliked_users = "disliked_users";
 
 // User collection fields.
 const String userInterestList = "interestsList";
-
-// Liked collection fields.
-const String likedUserIds = "ids";
-const String dislikedUserIds = "ids";
--- lib/feature_suggestion/ui/suggestion_card.dart
+++ lib/feature_suggestion/ui/suggestion_card.dart
@@ -1,13 +1,17 @@
-import 'package:flutter/foundation.dart';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:swipable_stack/swipable_stack.dart';
+import 'package:workout_buddy_finder/core/core.dart';
+import 'bloc/suggestions_bloc.dart';
 import 'empty_suggestion.dart';
 
 import '../domain/domain.dart';
 import 'suggestions_card_item.dart';
-import 'suggsetion_action.dart';
+import 'suggestion_action.dart';
 
-class SuggestionCard extends StatelessWidget {
+class SuggestionCard extends StatefulWidget {
   SuggestionCard({
     Key? key,
     required List<Suggestion> suggestion,
@@ -15,55 +19,105 @@ class SuggestionCard extends StatelessWidget {
         super(key: key);
 
   final List<Suggestion> _suggestions;
+
+  @override
+  State<SuggestionCard> createState() => _SuggestionCardState();
+}
+
+class _SuggestionCardState extends State<SuggestionCard> {
   final SwipableStackController _controller = SwipableStackController();
+  final List<Suggestion> suggestions = [];
+
   final _horizontalSwipeThreshold = 0.8;
+
   final _verticalSwipeThreshold = 0.8;
 
-  void _onSwipeCompleted(index, direction) {
-    final itemIndex = index % _suggestions.length;
-    if (kDebugMode) {
-      print('$itemIndex, $direction');
+  final _defaultItemCount = 3;
+
+  @override
+  void initState() {
+    super.initState();
+    suggestions.addAll(widget._suggestions);
+  }
+
+  void _onSwipeCompleted(
+    BuildContext context,
+    int index,
+    SwipeDirection direction,
+  ) {
+    final itemIndex = index % suggestions.length;
+    if (direction == SwipeDirection.left) {
+      context.read<SuggestionsBloc>().add(SuggestionsEvent.dislikeUser(
+            context.read<AppUser>(),
+            suggestions[itemIndex].userId,
+          ));
+    } else if (direction == SwipeDirection.right) {
+      context.read<SuggestionsBloc>().add(SuggestionsEvent.likeUser(
+            context.read<AppUser>(),
+            suggestions[itemIndex].userId,
+          ));
     }
   }
 
   Widget _buildSuggestionContent(_, properties) {
-    final itemIndex = properties.index % _suggestions.length;
+    final itemIndex = properties.index % suggestions.length;
 
     return Stack(children: [
-      SuggestionsCardItem(suggestion: _suggestions[itemIndex]),
+      SuggestionsCardItem(suggestion: suggestions[itemIndex]),
       // More custom overlay possible than with overlayBuilder.
     ]);
   }
 
+  void _listenToSuggestionBlocState(BuildContext _, SuggestionsState state) {
+    state.maybeWhen(
+      interactionSuccess: (userId) {
+        setState(() {
+          suggestions.removeWhere((e) => e.userId == userId);
+        });
+      },
+      orElse: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    if (_suggestions.isEmpty) {
+    if (suggestions.isEmpty) {
       return EmptySuggestion();
     }
 
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: SwipableStack(
-            controller: _controller,
-            detectableSwipeDirections: const {
-              SwipeDirection.left,
-              SwipeDirection.right,
-            },
-            stackClipBehaviour: Clip.none,
-            onSwipeCompleted: _onSwipeCompleted,
-            horizontalSwipeThreshold: _horizontalSwipeThreshold,
-            verticalSwipeThreshold: _verticalSwipeThreshold,
-            builder: _buildSuggestionContent,
+    return BlocListener<SuggestionsBloc, SuggestionsState>(
+      listener: _listenToSuggestionBlocState,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: SwipableStack(
+              itemCount: min(_defaultItemCount, suggestions.length),
+              controller: _controller,
+              detectableSwipeDirections: const {
+                SwipeDirection.left,
+                SwipeDirection.right,
+              },
+              stackClipBehaviour: Clip.none,
+              onSwipeCompleted: (index, direction) =>
+                  _onSwipeCompleted(context, index, direction),
+              horizontalSwipeThreshold: _horizontalSwipeThreshold,
+              verticalSwipeThreshold: _verticalSwipeThreshold,
+              builder: _buildSuggestionContent,
+            ),
           ),
-        ),
-        Positioned(
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: SuggestionAction(),
-        ),
-      ],
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: SuggestionAction(
+              onLike: () =>
+                  _controller.next(swipeDirection: SwipeDirection.right),
+              onDislike: () =>
+                  _controller.next(swipeDirection: SwipeDirection.left),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
