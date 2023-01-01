3c0adffc68a5e15fe9edcd5a22dfbd370276ad65
Md Didarul islam <islamdidarmd@gmail.com>
2023-01-01 20:06:06 +0600

    Use arrayunion and arrayappend to update interest

--- lib/core/firestore_constants.dart
+++ lib/core/firestore_constants.dart
@@ -1,6 +1,12 @@
 // Collections.
 const String users = "users";
 const String interests = "interests";
+const String liked_users = "liked_users";
+const String disliked_users = "disliked_users";
 
 // User collection fields.
 const String userInterestList = "interestsList";
+
+// Liked collection fields.
+const String likedUserIds = "ids";
+const String dislikedUserIds = "ids";
