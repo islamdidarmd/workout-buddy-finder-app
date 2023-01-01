4b4ab5b003352b4a268476abda526f7e1d8ec8e5
Md Didarul islam <islamdidarmd@gmail.com>
2023-01-01 21:36:01 +0600

    Add sign out mechanism

--- lib/feature_auth/ui/bloc/auth_event.dart
+++ lib/feature_auth/ui/bloc/auth_event.dart
@@ -4,4 +4,5 @@ part of 'auth_bloc.dart';
 class AuthEvent with _$AuthEvent{
   const factory AuthEvent.initial() = _InitialEvent;
   const factory AuthEvent.signInWithGoogle(Position location) = _SignInWithGoogleEvent;
+  const factory AuthEvent.signOut() = _SignOutEvent;
 }
