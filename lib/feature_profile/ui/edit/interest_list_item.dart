21d22065f92ea63cae094e4e40d244530b07bd0b
Md Didarul islam <islamdidarmd@gmail.com>
2023-01-02 22:12:57 +0600

    Fix delay in interest selection

--- lib/feature_profile/ui/edit/interest_list_item.dart
+++ lib/feature_profile/ui/edit/interest_list_item.dart
@@ -5,21 +5,41 @@ import 'package:workout_buddy_finder/feature_profile/ui/bloc/profile_bloc.dart';
 import '../../../core/core.dart';
 import '../view_model/view_model.dart';
 
-class InterestListItem extends StatelessWidget {
+class InterestListItem extends StatefulWidget {
   const InterestListItem({
-    Key? key, required this.interestViewModel,
+    Key? key,
+    required this.interestViewModel,
   }) : super(key: key);
 
   final InterestViewModel interestViewModel;
 
+  @override
+  State<InterestListItem> createState() => _InterestListItemState();
+}
+
+class _InterestListItemState extends State<InterestListItem> {
+  bool _isSelected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _isSelected = widget.interestViewModel.isSelected;
+  }
+
   void _onChanged(BuildContext context, bool? isSelected) {
+    setState(() {
+      _isSelected = isSelected ?? false;
+    });
+
     final profileBloc = context.read<ProfileBloc>();
     final appUser = context.read<AppUser>();
 
     if (isSelected == true) {
-      profileBloc.add(ProfileEvent.addInterest(appUser, interestViewModel));
+      profileBloc
+          .add(ProfileEvent.addInterest(appUser, widget.interestViewModel));
     } else {
-      profileBloc.add(ProfileEvent.removeInterest(appUser,interestViewModel));
+      profileBloc
+          .add(ProfileEvent.removeInterest(appUser, widget.interestViewModel));
     }
   }
 
@@ -27,15 +47,16 @@ class InterestListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: CheckboxListTile(
-        value: interestViewModel.isSelected,
+        value: _isSelected,
         title: Row(
           children: [
             CircleAvatar(
               radius: 12,
-              foregroundImage: Image.network(interestViewModel.icon).image,
+              foregroundImage:
+                  Image.network(widget.interestViewModel.icon).image,
             ),
             const SizedBox(width: 8),
-            Text(interestViewModel.name),
+            Text(widget.interestViewModel.name),
           ],
         ),
         onChanged: (isSelected) => _onChanged(context, isSelected),
