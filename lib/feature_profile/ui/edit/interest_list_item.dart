21b3fc1bec75776a2efa15480e488c27de2ef34b
Md Didarul islam <islamdidarmd@gmail.com>
2023-01-02 22:17:18 +0600

    replace set state with value notifier

--- lib/feature_profile/ui/edit/interest_list_item.dart
+++ lib/feature_profile/ui/edit/interest_list_item.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:workout_buddy_finder/feature_profile/ui/bloc/profile_bloc.dart';
 
 import '../../../core/core.dart';
 import '../view_model/view_model.dart';
 
-class InterestListItem extends StatefulWidget {
+class InterestListItem extends HookWidget {
   const InterestListItem({
     Key? key,
     required this.interestViewModel,
@@ -13,53 +14,38 @@ class InterestListItem extends StatefulWidget {
 
   final InterestViewModel interestViewModel;
 
-  @override
-  State<InterestListItem> createState() => _InterestListItemState();
-}
-
-class _InterestListItemState extends State<InterestListItem> {
-  bool _isSelected = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _isSelected = widget.interestViewModel.isSelected;
-  }
-
   void _onChanged(BuildContext context, bool? isSelected) {
-    setState(() {
-      _isSelected = isSelected ?? false;
-    });
-
     final profileBloc = context.read<ProfileBloc>();
     final appUser = context.read<AppUser>();
 
     if (isSelected == true) {
-      profileBloc
-          .add(ProfileEvent.addInterest(appUser, widget.interestViewModel));
+      profileBloc.add(ProfileEvent.addInterest(appUser, interestViewModel));
     } else {
-      profileBloc
-          .add(ProfileEvent.removeInterest(appUser, widget.interestViewModel));
+      profileBloc.add(ProfileEvent.removeInterest(appUser, interestViewModel));
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    final checkboxValue = useState(interestViewModel.isSelected);
+
     return Container(
       child: CheckboxListTile(
-        value: _isSelected,
+        value: checkboxValue.value,
         title: Row(
           children: [
             CircleAvatar(
               radius: 12,
-              foregroundImage:
-                  Image.network(widget.interestViewModel.icon).image,
+              foregroundImage: Image.network(interestViewModel.icon).image,
             ),
             const SizedBox(width: 8),
-            Text(widget.interestViewModel.name),
+            Text(interestViewModel.name),
           ],
         ),
-        onChanged: (isSelected) => _onChanged(context, isSelected),
+        onChanged: (isSelected) {
+          checkboxValue.value = isSelected ?? false;
+          _onChanged(context, isSelected);
+        },
       ),
     );
   }
