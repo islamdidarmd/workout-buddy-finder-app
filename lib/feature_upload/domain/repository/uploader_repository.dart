19ab5786be71fa01a412ecad31e594246dff8f66
Md Didarul islam <islamdidarmd@gmail.com>
2023-01-03 23:23:47 +0600

    Implement replace image method

--- lib/feature_upload/domain/repository/uploader_repository.dart
+++ lib/feature_upload/domain/repository/uploader_repository.dart
@@ -4,10 +4,10 @@ import 'package:either_dart/either.dart';
 import 'package:workout_buddy_finder/core/core.dart';
 
 abstract class UploaderRepository {
-  Future<Either<String, AppError>> replaceImage({
-    required AppUser appUser,
-    required File image,
+  Future<Either<void, AppError>> replaceImage({
     required String url,
+    required String path,
+    required File image,
   });
 
   Future<Either<String, AppError>> uploadImage({
