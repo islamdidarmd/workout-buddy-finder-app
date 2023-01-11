5026bd303f08fbaddc71fd5c34db83397531eb86
Md Didarul islam <islamdidarmd@gmail.com>
2023-01-11 22:40:15 +0600

    Load name and profile pictures of chat item

---
+++ lib/feature_messaging/ui/messges/message_list_item.dart
@@ -0,0 +1,76 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:workout_buddy_finder/core/core.dart';
+import 'package:workout_buddy_finder/feature_auth/data/model/model.dart';
+import '../../domain/domain.dart';
+
+class MessageListItem extends HookWidget {
+  const MessageListItem({
+    Key? key,
+    required this.chat,
+    required this.appUser,
+  }) : super(key: key);
+
+  final Chat chat;
+  final AppUser appUser;
+  final double _height = 60.0;
+
+  Future<List<AppUser>> get _participants async {
+    final participants = <AppUser>[];
+    final loggedInUser = appUser;
+
+    for (String userId in chat.participants) {
+      if (userId == loggedInUser.userId) {
+        continue;
+      }
+
+      final profileQuery = FirebaseFirestore.instance
+          .collection(col_users)
+          .doc(userId)
+          .withConverter(
+            fromFirestore: (snapshot, _) =>
+                AppUserModel.fromJson(snapshot.data()!).toEntity([]),
+            toFirestore: (_, __) => {},
+          );
+      final appUser = await profileQuery.get();
+      participants.add(appUser.data()!);
+    }
+
+    return participants;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final participantsFuture = useMemoized(() => _participants);
+    final participantsSnapshot = useFuture(participantsFuture);
+    if (participantsSnapshot.connectionState == ConnectionState.waiting) {
+      return LinearProgressIndicator();
+    } else if (participantsSnapshot.hasData) {
+      final participant = participantsSnapshot.requireData.first;
+
+      return Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
+          child: SizedBox(
+            height: _height,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  foregroundImage:
+                      CachedNetworkImageProvider(participant.profilePicture),
+                ),
+                const SizedBox(width: 16),
+                mediumBody(context, participant.name),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Container();
+  }
+}
