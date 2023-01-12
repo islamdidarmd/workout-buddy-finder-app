88cb779f5d0a93dbddd89e228f9144925fd17920
Md Didarul islam <islamdidarmd@gmail.com>
2023-01-12 18:00:45 +0600

    Add chat room list and input field

--- lib/feature_messaging/domain/entity/entities.dart
+++ lib/feature_messaging/domain/entity/entities.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
+import 'package:workout_buddy_finder/core/core.dart';
 
 part 'chat_room.dart';
 part 'chat_user.dart';
+part 'chat_message.dart';
 
 part 'entities.freezed.dart';
 part 'entities.g.dart';
