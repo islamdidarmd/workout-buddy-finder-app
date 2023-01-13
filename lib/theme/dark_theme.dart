6afd17bd5e0ef20da92f98d14b0588c55a52b084
Md Didarul islam <islamdidarmd@gmail.com>
2023-01-13 21:43:10 +0600

    Update color scheme

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-const _seedColor = Color(0xFFFE5D8D);
+const _seedColor = Color(0xff410081);
 final baseTheme =
     ThemeData(colorSchemeSeed: _seedColor, brightness: Brightness.dark, useMaterial3: true);
 final darkTheme = baseTheme.copyWith(
