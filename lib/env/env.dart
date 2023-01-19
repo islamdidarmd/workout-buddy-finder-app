e68539908f3b0bbbcc26ab94bb1993d3a4072849
Md Didarul islam <islamdidarmd@gmail.com>
2023-01-19 23:00:01 +0600

    Add theme changing in settings

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:workout_buddy_finder/firebase_options.dart';
+import 'package:workout_buddy_finder/theme/theme_manager.dart';
 import 'constants.dart';
 import 'package:firebase_core/firebase_core.dart';
 import '../app/wbf_app.dart';
@@ -12,9 +13,10 @@ class Env {
   final EnvType envType = defaultEnv;
   final String appName = defaultAppName;
 
-  void init({required GetIt slInstance}) {
-    WidgetsFlutterBinding.ensureInitialized();
+  Future<void> init({required GetIt slInstance}) async {
+    final binding = WidgetsFlutterBinding.ensureInitialized();
     configureDependencies(getIt: slInstance);
+    await ThemeManager.instance.init();
   }
 
   Future<void> startApplication() async {
