5171b2c24074456319ad14bcd67f161080fa65d0
Md Didarul islam <islamdidarmd@gmail.com>
2023-01-30 21:41:53 +0600

    Add interest items animation

--- lib/core/ui/shared/user_interest_section.dart
+++ lib/core/ui/shared/user_interest_section.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import '../../core.dart';
 
-class UserInterestSection extends StatelessWidget {
+class UserInterestSection extends StatefulWidget {
   const UserInterestSection({
     Key? key,
     required this.appUser,
   }) : super(key: key);
 
   final AppUser appUser;
+
+  @override
+  State<UserInterestSection> createState() => _UserInterestSectionState();
+}
+
+class _UserInterestSectionState extends State<UserInterestSection> {
+  final interestList = <String>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _fillInterestListWithDelay();
+  }
+
+  Future<void> _fillInterestListWithDelay() async {
+    final result = await Future.delayed(300.milliseconds);
+    for (String interest in widget.appUser.interestList) {
+      final result = await Future.delayed(200.milliseconds);
+      if (mounted) {
+        setState(() {
+          interestList.add(interest);
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,8 +49,12 @@ class UserInterestSection extends StatelessWidget {
               const SizedBox(height: 8),
               Wrap(
                 spacing: 4,
-                children: appUser.interestList
-                    .map((id) => InterestChip(interestId: id))
+                children: interestList
+                    .map(
+                      (id) => InterestChip(interestId: id)
+                          .animate()
+                          .fadeIn(duration: 200.milliseconds),
+                    )
                     .toList(),
               ),
             ],
