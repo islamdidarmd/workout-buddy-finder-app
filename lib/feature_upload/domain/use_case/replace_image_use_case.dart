5a843e88c05cd8cd375c71963787b8aaeedcbd26
Md Didarul islam <islamdidarmd@gmail.com>
2023-02-10 22:33:51 +0600

    Replace image code update

--- lib/feature_upload/domain/use_case/replace_image_use_case.dart
+++ lib/feature_upload/domain/use_case/replace_image_use_case.dart
@@ -7,20 +7,11 @@ import 'package:injectable/injectable.dart';
 class ReplaceImageUseCase {
   Future<String?> call({
     required String url,
-    required String path,
     required File image,
-    String? fileName,
   }) async {
     try {
       final fileRef = FirebaseStorage.instance.refFromURL(url);
-      final imageName = fileName ?? fileRef.name;
-
-      await fileRef.delete();
-      final newRef =
-      FirebaseStorage.instance.ref().child(path).child(imageName);
-
-
-      final uploaderTask = await newRef.putFile(image);
+      final uploaderTask = await fileRef.putFile(image);
 
       return await fileRef.getDownloadURL();
     } catch (e) {
