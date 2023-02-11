6305c172c9831fb9196ccac76ad756124a3d2481
Md Didarul islam <islamdidarmd@gmail.com>
2023-02-11 13:54:03 +0600

    Update service locator generation

--- lib/di/service_locator.dart
+++ lib/di/service_locator.dart
@@ -2,14 +2,9 @@ import 'package:injectable/injectable.dart';
 import 'package:get_it/get_it.dart';
 import 'service_locator.config.dart';
 
-late GetIt sl;
+final GetIt sl = GetIt.instance;
 
-@InjectableInit(
-  asExtension: false,
-  initializerName: r'$InitGetIt',
-  preferRelativeImports: true,
-)
-void configureDependencies({required GetIt getIt}) {
-  sl = getIt;
-  final result = $InitGetIt(sl);
+@InjectableInit(initializerName: 'initGetIt', preferRelativeImports: true)
+void configureDependencies() {
+  final result = sl.initGetIt();
 }
