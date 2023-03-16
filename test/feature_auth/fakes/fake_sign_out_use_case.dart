c7459b87deb0c320e79bb0df44727d0f55be8632
Md Didarul islam <islamdidarmd@gmail.com>
2023-03-16 16:53:49 +0600

    Added signed out state test

--- test/feature_auth/fakes/fake_sign_out_use_case.dart
+++ test/feature_auth/fakes/fake_sign_out_use_case.dart
@@ -3,8 +3,16 @@ import 'package:workout_buddy_finder/core/error/app_error.dart';
 import 'package:workout_buddy_finder/feature_auth/feature_auth.dart';
 
 class FakeSignOutUseCase implements SignOutUseCase {
+  final AppError? error;
+
+  const FakeSignOutUseCase(this.error);
+
   @override
-  Future<Either<void, AppError>> execute() {
-    return Future.value(null);
+  Future<Either<void, AppError>> execute() async {
+    if (error != null) {
+      return Right(UnknownError());
+    }
+
+    return Left(null);
   }
 }
