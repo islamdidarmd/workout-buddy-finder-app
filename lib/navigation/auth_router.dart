76c770f875255d82fcf839188f895e30c7aa03b6
Md Didarul islam <islamdidarmd@gmail.com>
2023-05-07 08:17:09 +0600

    Fixed analysis

--- lib/navigation/auth_router.dart
+++ lib/navigation/auth_router.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-import '../features/auth/ui/sign_in_page.dart';
-import '../features/auth/ui/sign_up_page.dart';
+import '../../features/auth/ui/sign_in_page.dart';
+import '../../features/auth/ui/sign_up_page.dart';
 import 'app_route.dart';
 
 final _authNavigatorKey = GlobalKey<NavigatorState>();
